fc87ce3c6849039367e8d459d648167159387148
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-02-20 11:00:27 +0700

    initiate file

---
+++ src/main(structure2).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct Person
+{
+    char name[40];
+    int age;
+    float salary;
+};
+
+int main()
+{
+    Person p1;
+    cout << "enter your name:\n ";
+    cin.get(p1.name, 40);
+    cout << "enter your age: \n";
+    cin >> p1.age;
+    cout << "enter your salary: \n";
+    cin >> p1.salary;
+
+    cout << "your name is " << p1.name << ".\n";
+    cout << "your age is " << p1.age << ".\n";
+    cout << "your salary is " << p1.salary << ".\n";
+
+    return 0;
+}
